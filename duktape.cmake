15390a0854c49a2a3c7b3b50fd5c2dcc95551dbd
Sami Vaarala <sami.vaarala@iki.fi>
2016-07-28 17:58:36 +0300

    Add logging extra when using 2.0

--- duktape.cmake
+++ duktape.cmake
@@ -1,10 +1,22 @@
 set(DUKTAPEDIR ${CMAKE_CURRENT_LIST_DIR}/lib/duktape)
 
+if(EXISTS ${DUKTAPEDIR}/extras/logging/duk_logging.c)
+include_directories(
+  ${DUKTAPEDIR}/src
+  ${DUKTAPEDIR}/extras/logging
+)
+add_library(duktape STATIC
+  ${DUKTAPEDIR}/src/duktape.c
+  ${DUKTAPEDIR}/extras/logging/duk_logging.c
+)
+else()
 include_directories(
   ${DUKTAPEDIR}/src
 )
-
-add_library(duktape STATIC ${DUKTAPEDIR}/src/duktape.c)
+add_library(duktape STATIC
+  ${DUKTAPEDIR}/src/duktape.c
+)
+endif()
 
 if("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
   target_link_libraries(duktape
